773cec47ad0f3a368a4a7d595dc1ff60fcb87ec6
Gabriel Borela <gabrielborela07@gmail.com>
2025-11-10 14:19:57 -0300

    novo app

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDwo5ScsP8TiwiGPuZB-GBdxSrHrwb0n70",
-    authDomain: "portal-assinaturas-ai.firebaseapp.com",
-    projectId: "portal-assinaturas-ai",
-    storageBucket: "portal-assinaturas-ai.firebasestorage.app",
-    messagingSenderId: "473640121277",
-    appId: "1:473640121277:web:0b84a7acb609bcebe498d4"
-  };
-  
+  apiKey: "AIzaSyDwo5ScsP8TiwiGPuZB-GBdxSrHrwb0n70",
+  authDomain: "portal-assinaturas-ai.firebaseapp.com",
+  projectId: "portal-assinaturas-ai",
+  storageBucket: "portal-assinaturas-ai.appspot.com",
+  messagingSenderId: "473640121277",
+  appId: "1:473640121277:web:0b84a7acb609bcebe498d4"
+};
+
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
